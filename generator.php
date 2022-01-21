57b0584eb75c4a9cd3278c849dbc90ed5c8aa779
mitchoz <3161448+mitchoz@users.noreply.github.com>
2022-01-21 13:11:44 +0100

    Submitting my NFT Generatooor to GIT

---
+++ generator.php
@@ -0,0 +1,91 @@
+<?php
+
+include("vars.php");
+
+
+for($i=$startnumber;$i<$maxSupply+$startnumber;$i++)
+{
+    if($addNumbers)
+        $vname = $name." #".$i."/".$maxSupply;
+    else $vname = $name;
+
+    if($printoutput)
+    {
+        echo "-----------";
+        echo "\n";
+        echo "Creating NFT Metadata for:";
+        echo "\n";
+        echo $vname;
+        echo "\n";
+        echo $description;
+        echo "\n";
+        echo "\n";
+        echo $image;
+        echo "\n";
+        echo $image_url;
+        echo "\n";
+        echo $thumbnail_url;
+        echo "\n";
+        echo "\n";
+
+        echo "*Traits:*";
+        echo "\n";
+        foreach($traits as $key=>$value)
+        {
+            echo $key.":".$value;
+            echo "\n";
+        }
+        echo "\n";
+    }
+
+    $obj = new stdClass();
+    $obj->name = $vname;
+    $obj->description = $description;
+    $obj->image = $image;
+    $obj->image_url = $image_url;
+    $obj->thumbnail_url = $thumbnail_url;
+
+    if(isset($animation_url) && $animation_url != "")
+        $obj->animation_url = $animation_url;
+
+    if(isset($external_url) && $external_url != "")
+        $obj->external_url = $external_url;
+
+    
+    if(count($traits) > 0)
+    {
+        $obj->attributes = [];
+        foreach($traits as $key=>$value)
+        {
+                $trait = new stdClass();
+                $trait->trait_type = $key;
+                $trait->value = $value;
+                array_push($obj->attributes,$trait);
+        }
+    }
+    
+    
+    $json = json_encode($obj);
+
+    echo $json;
+
+    if($addNumberToFilename)
+        $writeto = $filenameroot."_".$i;
+    else
+        $writeto = $filenameroot;
+
+    if (file_put_contents($dir."/export/".$writeto.".json", $json))
+        echo "JSON file created for #".$i;
+    else 
+        echo "Error creating json file for #".$i;
+
+    
+
+
+}
+
+
+
+
+
+?>
---
+++ vars.php
@@ -0,0 +1,39 @@
+<?php
+
+$dir = __DIR__;
+
+//Generator Config
+
+$filenameroot = "";
+$addNumberToFilename = true;
+$printoutput = true; //Print individual NFT data to console while generator is running
+$addNumbers = true; //Add the version number to the name
+
+
+//Mandatory Input
+$maxSupply = 5; //This is the max supply of tokens that can be created through sharing
+$startnumber = 1; //The number of the first NFT. Generator will run until $maxSupply+$startnumber
+
+$name = "Your name";
+$description = "Your project description";
+
+//(Best is to upload the following data onto IPFS for safe storage)
+$image = "";
+$image_url = "";
+$thumbnail_url = "";
+
+//Optional Input
+$animation_url = "";
+$external_url = "";
+
+//Additional Properties and traits
+$traits = []; //["propertyA" => 1, "propertyB" => 2];
+
+
+
+
+
+
+
+
+?>
